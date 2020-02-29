86ce92b5429f8416dccca7eab6373bf185c91785
Sparsh Bansal <sbansal@olin.edu>
2020-02-29 12:03:26 -0500

    Haha

--- lab3/Lab3.m
+++ lab3/Lab3.m
@@ -20,7 +20,6 @@ I_s = exp(fit(2));
 
 theoretical_Ic = I_s*exp((Experiment_1.Vb - 0)./U_T);
 
-
 figure
 semilogy(Experiment_1.Vb,Experiment_1.Ic,'b*')
 hold on
@@ -30,30 +29,31 @@ semilogy(Experiment_1.Vb,theoretical_Ic,'g*')
 title('Experimental I-V Characteristic of a Bipolar Transistor Terminals')
 xlabel('Voltage [V]')
 ylabel('Current [A]')
-legend('Experimental Collector Current','Experimental Base Current','Base Current Line of Best Fit','Location','Southeast')
+legend('Experimental Collector Current','Experimental Base Current','Base Current Line of Best Fit','Theoretical Collector Current','Theoretical Base Current','Location','Southeast')
 grid on
 hold off
 
-figure
-semilogy(Experiment_1.Vb(16:55),Experiment_1.Ic(16:55),'b*')
-hold on
-semilogy(Experiment_1.Vb(16:55),Experiment_1.Ib(16:55),'r*')
-semilogy(x(16:end),line(16:end),'g-','LineWidth',1.5);
-semilogy(Experiment_1.Vb(16:55),theoretical_Ic(16:55),'g*')
-title('Experimental I-V Characteristic of a Bipolar Transistor Terminals')
-xlabel('Voltage [V]')
-ylabel('Current [A]')
-legend('Experimental Collector Current','Experimental Base Current','Base Current Line of Best Fit','Location','Southeast')
-grid on
-hold off
+% If we want to plot a range of Currents
+% figure
+% semilogy(Experiment_1.Vb(16:55),Experiment_1.Ic(16:55),'b*')
+% hold on
+% semilogy(Experiment_1.Vb(16:55),Experiment_1.Ib(16:55),'r*')
+% semilogy(x(16:end),line(16:end),'g-','LineWidth',1.5);
+% semilogy(Experiment_1.Vb(16:55),theoretical_Ic(16:55),'g*')
+% title('Experimental I-V Characteristic of a Bipolar Transistor Terminals')
+% xlabel('Voltage [V]')
+% ylabel('Current [A]')
+% legend('Experimental Collector Current','Experimental Base Current','Base Current Line of Best Fit','Location','Southeast')
+% grid on
+% hold off
 
 figure
 semilogx(Experiment_1.Ib,Experiment_1.Ic./Experiment_1.Ib,'b*')
 hold on
 title('Experimental I-V Characteristic of a Bipolar Transistor Terminals')
 xlabel('Base Current [A]')
-ylabel('Beta')
-legend('Experimental Collector Current','Experimental Base Current','Base Current Line of Best Fit','Location','Southeast')
+ylabel('\Beta')
+legend('Experimental \Beta','Location','Southeast')
 grid on
 hold off
 
@@ -63,7 +63,7 @@ hold on
 title('Experimental I-V Characteristic of a Bipolar Transistor Terminals')
 xlabel('Base Current [A]')
 ylabel('R_{b}')
-legend('Experimental Collector Current','Experimental Base Current','Base Current Line of Best Fit','Location','Southeast')
+legend('Incremental Base Resistance(R_{b})','Theoretical Incremental Base Resistance(R_{b})','Location','Southeast')
 grid on
 hold off
 
@@ -73,7 +73,7 @@ hold on
 title('Experimental I-V Characteristic of a Bipolar Transistor Terminals')
 xlabel('Collector Current [A]')
 ylabel('g_{m}')
-legend('Experimental Collector Current','Experimental Base Current','Base Current Line of Best Fit','Location','Southeast')
+legend('Experimental Incremental Transconductance Gain (g_{m})','Theoretical Incremental Transconductance Gain (g_{m})','Location','Southeast')
 grid on
 hold off
 
