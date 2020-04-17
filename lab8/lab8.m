2c853d5765fc7151d17b3b4d31ae24469d2c54f5
ThomasJagielski <42981129+ThomasJagielski@users.noreply.github.com>
2020-04-17 17:18:00 -0500

    Add offset votlage plot for experiment 3

--- lab8/lab8.m
+++ lab8/lab8.m
@@ -111,7 +111,6 @@ hold off
 
 x_intercept = (-1*Exp2_MI.Vout_fit(2))/Exp2_MI.Vout_fit(1);
 
-%%
 Exp2_MI.Vdm_fit = polyfit(Exp2_MI.Vdm(46:55,1),Exp2_MI.Vdm(46:55,2),1);
 Exp2_MI.Vdm_line = Exp2_MI.Vdm_fit(1)*(Exp2_MI.Vdm(46:55,1)) + Exp2_MI.Vdm_fit(2);
 Exp2_MI.Vdm_slope = (Exp2_MI.Vdm_line(end) - Exp2_MI.Vdm_line(1))/(Exp2_MI.Vdm(55,1)-Exp2_MI.Vdm(46,1));
@@ -122,7 +121,104 @@ hold on
 grid on
 plot(Exp2_MI.Vdm(46:55,1)-2.5,Exp2_MI.Vdm_line,'r-')
 title('Current Voltage Characteristics for NMOS Differential Pair')
-%legend('Experimental Data','Fit Data','Location','Southeast','NumColumns',1)
+legend('Experimental Data','Fit Data','Location','Southeast','NumColumns',1)
 xlabel('V_{dm} [V]')
 ylabel('I_{out} [A]')
+hold off
+
+
+% Strong Inversion Characteristics
+Exp2_SI.Vout = (importdata("Experiment2_SI_Vout_sweep_Iout.txt"));
+Exp2_SI.Vdm = (importdata("Experiment2_SI_Vdm_about_2_5_Iout.txt"));
+
+% Fit
+Exp2_SI.Vout(:,2) = -1 * Exp2_SI.Vout(:,2);
+Exp2_SI.Vout_fit = polyfit(Exp2_SI.Vout(150:460,1),Exp2_SI.Vout(150:460,2),1);
+Exp2_SI.Vout_line = Exp2_SI.Vout_fit(1)*(Exp2_SI.Vout(150:460,1)) + Exp2_SI.Vout_fit(2);
+Exp2_SI.Vout_slope = (Exp2_SI.Vout_line(end) - Exp2_SI.Vout_line(1))/(Exp2_SI.Vout(460,1)-Exp2_SI.Vout(150,1));
+
+figure()
+axis([0 5 -1e-6 1e-6])
+plot(Exp2_SI.Vout(130:490,1),Exp2_SI.Vout(130:490,2),'b.')
+hold on
+grid on
+plot(Exp2_SI.Vout(150:460,1),Exp2_SI.Vout_line,'r-')
+title('Current Voltage Characteristics for NMOS Differential Pair, I_{b} in Strong Inversion')
+legend('Experimental Data','Fit Data','Location','Northwest','NumColumns',1)
+xlabel('V_{out} [V]')
+ylabel('I_{out} [A]')
+hold off
+
+SI_x_intercept = (-1*Exp2_SI.Vout_fit(2))/Exp2_SI.Vout_fit(1);
+Exp2_SI.Vdm_fit = polyfit(Exp2_SI.Vdm(35:61,1),Exp2_SI.Vdm(35:61,2),1);
+Exp2_SI.Vdm_line = Exp2_SI.Vdm_fit(1)*(Exp2_SI.Vdm(35:61,1)) + Exp2_SI.Vdm_fit(2);
+Exp2_SI.Vdm_slope = (Exp2_SI.Vdm_line(end) - Exp2_SI.Vdm_line(1))/(Exp2_SI.Vdm(61,1)-Exp2_SI.Vdm(35,1));
+
+figure()
+plot(Exp2_SI.Vdm(:,1)-2.5,Exp2_SI.Vdm(:,2),'b.')
+hold on
+grid on
+plot(Exp2_SI.Vdm(35:61,1)-2.5,Exp2_SI.Vdm_line,'r-')
+title('Current Voltage Characteristics for NMOS Differential Pair, I_{b} in Strong Inversion')
+legend('Experimental Data','Fit Data','Location','Southeast','NumColumns',1)
+xlabel('V_{dm} [V]')
+ylabel('I_{out} [A]')
+hold off
+
+
+
+%% Experiment 3
+Exp3_MI = (importdata("Experiment3_Threshold.txt"));
+Exp3_SI = (importdata("Experiment3_SI.txt"));
+
+Exp3_MI_fit = polyfit(Exp3_MI(100:400,1),Exp3_MI(100:400,2),1);
+Exp3_MI_line = Exp3_MI_fit(1)*(Exp3_MI(100:400,1)) + Exp3_MI_fit(2);
+Exp3_MI_slope = (Exp3_MI_line(end) - Exp3_MI_line(1))/(Exp3_MI(400,1)-Exp3_MI(100,1));
+
+figure
+plot(Exp3_MI(1:5:end,1),Exp3_MI(1:5:end,2),'.')
+hold on
+grid on
+plot(Exp3_MI(100:400,1),Exp3_MI_line,'r-')
+title('Unity_Gain Follower for NMOS Differential Pair, I_{b} at Threshold')
+legend('Experimental Data','Fit Data','Location','Southeast','NumColumns',1)
+xlabel('V_{in} [V]')
+ylabel('V_{out} [V]')
+hold off
+
+figure
+plot(Exp3_MI(1:5:end,1),Exp3_MI(1:5:end,2)-Exp3_MI(1:5:end,1),'.')
+hold on
+grid on
+title('Unity_Gain Follower for NMOS Differential Pair, I_{b} at Threshold')
+%legend('Experimental Data','Fit Data','Location','Southeast','NumColumns',1)
+xlabel('V_{in} [V]')
+ylabel('V_{out} [V]')
+hold off
+
+
+% Above Threshold
+Exp3_SI_fit = polyfit(Exp3_SI(100:400,1),Exp3_SI(100:400,2),1);
+Exp3_SI_line = Exp3_SI_fit(1)*(Exp3_SI(100:400,1)) + Exp3_SI_fit(2);
+Exp3_SI_slope = (Exp3_SI_line(end) - Exp3_SI_line(1))/(Exp3_SI(400,1)-Exp3_SI(100,1));
+
+figure
+plot(Exp3_SI(1:5:end,1),Exp3_SI(1:5:end,2),'.')
+hold on
+grid on
+plot(Exp3_SI(100:400,1),Exp3_SI_line,'r-')
+title('Unity_Gain Follower for NMOS Differential Pair, I_{b} in Strong Inversion')
+legend('Experimental Data','Fit Data','Location','Southeast','NumColumns',1)
+xlabel('V_{in} [V]')
+ylabel('V_{out} [V]')
+hold off
+
+figure
+plot(Exp3_SI(1:5:end,1),Exp3_SI(1:5:end,2)-Exp3_SI(1:5:end,1),'.')
+hold on
+grid on
+title('Unity_Gain Follower for NMOS Differential Pair, I_{b} at Threshold')
+%legend('Experimental Data','Fit Data','Location','Southeast','NumColumns',1)
+xlabel('V_{in} [V]')
+ylabel('V_{out} [V]')
 hold off
