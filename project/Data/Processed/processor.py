8804579da22176ffe43d5c83416e4e9d241b442a
Sparsh Bansal <sbansal@olin.edu>
2020-05-03 03:44:55 +0530

    Adding recent updates

---
+++ project/Data/Processed/processor.py
@@ -0,0 +1,28 @@
+# import unidecode
+
+# def remove_non_ascii(text):
+#     return unidecode(unicode(text, encoding = "utf-8"))
+
+if __name__ == "__main__":
+    f = open("Circuit_Indirect_Bode_1200f.txt", "r")
+    g = open("Circuit_Indirect_Bode_1200f_p.txt", "w")
+    for line in f:
+        # line = remove_non_ascii(line)
+        for char in line:
+            if ord(char) >= 128:
+                continue
+            if char == ')' or char == 'd' or char == 'B' or char == ' ':
+                char = ''
+            if char == '(':
+                char = ','
+            g.write(char)
+
+
+
+f.close()
+g.close()
+    
+
+
+
+
---
+++ project/project.m
@@ -0,0 +1,105 @@
+%% Indirect
+
+In.Vout_1200 = (importdata("Circuit_Indirect_Vout_1200f.txt"));
+In.Vout_800 = (importdata("Circuit_Indirect_Vout_800f.txt"));
+In.Vout_80 = (importdata("Circuit_Indirect_Vout_80f.txt"));
+[a, b, c] = importfile("C:\Users\sbansal\Desktop\Circuits\MicroelectronicCircuits\project\Data\Processed\Circuit_Indirect_Bode_80f_p.txt", [1, Inf]);
+In.Bode_80 = [a b c];
+[a, b, c] = importfile("C:\Users\sbansal\Desktop\Circuits\MicroelectronicCircuits\project\Data\Processed\Circuit_Indirect_Bode_800f_p.txt", [1, Inf]);
+In.Bode_800 = [a b c];
+[a, b, c] = importfile("C:\Users\sbansal\Desktop\Circuits\MicroelectronicCircuits\project\Data\Processed\Circuit_Indirect_Bode_1200f_p.txt", [1, Inf]);
+In.Bode_1200 = [a b c];
+
+
+figure()
+plot(In.Vout_1200(1:750,1),In.Vout_1200(1:750,2),'r.')
+hold on
+plot(In.Vout_1200(1:750,1),In.Vout_1200(1:750,3),'b.')
+legend('V_{in}','V_{out}','Location','Southeast','NumColumns',1)
+xlabel('time [s]')
+ylabel('Votlage [V]')
+hold off
+
+figure()
+plot(In.Vout_800(1:700,1),In.Vout_800(1:700,2),'r.')
+hold on
+plot(In.Vout_800(1:700,1),In.Vout_800(1:700,3),'b.')
+legend('V_{in}','V_{out}','Location','Southeast','NumColumns',1)
+xlabel('time [s]')
+ylabel('Votlage [V]')
+hold off
+
+figure()
+plot(In.Vout_80(1:950,1),In.Vout_80(1:950,2),'r.')
+hold on
+plot(In.Vout_80(1:950,1),In.Vout_80(1:950,3),'b.')
+legend('V_{in}','V_{out}','Location','Southeast','NumColumns',1)
+xlabel('time [s]')
+ylabel('Votlage [V]')
+hold off
+
+figure()
+subplot(2,1,1)
+semilogx(In.Bode_80(1:end,1),In.Bode_80(1:end,2),'r.')
+hold on
+x = logspace(5, 8);
+plot(x, x*0, 'g')
+title('Bode Plot for C = 80f [F]')
+hold off
+axis([1e5 1e8 -60 20])
+xlabel('Frequency [Hz]')
+ylabel('Amplitude of Gain [dB]')
+subplot(2,1,2)
+semilogx(In.Bode_80(1:end,1),In.Bode_80(1:end,3),'b.')
+hold on
+x = logspace(5, 8);
+plot(x, x*0-180, 'g')
+hold off
+axis([1e5 1e8 -200 200])
+xlabel('Frequency [Hz]')
+ylabel('Phase Angle [°]')
+
+figure()
+subplot(2,1,1)
+semilogx(In.Bode_800(1:end,1),In.Bode_800(1:end,2),'r.')
+hold on
+x = logspace(5, 8);
+plot(x, x*0, 'g')
+title('Bode Plot for C = 800f [F]')
+hold off
+axis([1e5 1e8 -60 20])
+xlabel('Frequency [Hz]')
+ylabel('Amplitude of Gain [dB]')
+subplot(2,1,2)
+semilogx(In.Bode_800(1:end,1),In.Bode_800(1:end,3),'b.')
+hold on
+x = logspace(5, 8);
+plot(x, x*0-180, 'g')
+hold off
+axis([1e5 1e8 -200 200])
+xlabel('Frequency [Hz]')
+ylabel('Phase Angle [°]')
+
+figure()
+subplot(2,1,1)
+semilogx(In.Bode_1200(1:end,1),In.Bode_1200(1:end,2),'r.')
+hold on
+x = logspace(5, 8);
+plot(x, x*0, 'g')
+title('Bode Plot for C = 1200f [F]')
+hold off
+axis([1e5 1e8 -60 20])
+xlabel('Frequency [Hz]')
+ylabel('Amplitude of Gain [dB]')
+subplot(2,1,2)
+semilogx(In.Bode_1200(1:end,1),In.Bode_1200(1:end,3),'b.')
+hold on
+x = logspace(5, 8);
+plot(x, x*0-180, 'g')
+hold off
+axis([1e5 1e8 -200 200])
+xlabel('Frequency [Hz]')
+ylabel('Phase Angle [°]')
+
+%% Uncompensated
+
